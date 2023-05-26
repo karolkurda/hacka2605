fd88b5d52c2fc6ffc66104014499eb457fc72f5b
Bartosz Kozlowski <bartikozlowski@gmail.com>
2023-05-26 19:31:50 +0200

    setup

---
+++ Ekonfident/src/main/java/com/example/ekonfident/EkonfidentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekonfident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkonfidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkonfidentApplication.class, args);
+	}
+
+}
---
+++ Ekonfident/src/test/java/com/example/ekonfident/EkonfidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ekonfident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkonfidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
